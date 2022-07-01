e500ab8ac0d2992d01bccffc1bcba9e1365b102d
yajvanth <108358675+yajvanth@users.noreply.github.com>
2022-07-01 12:01:45 +0530

    Task001-290622920
    one.java file added

---
+++ one.java
@@ -0,0 +1,12 @@
+import java.lang.*;
+
+class one
+{
+  public static void main(String[] args)
+  {
+    system.out.printin("welcome to java application-1");
+    system.out.printin("welcome to java application-1");
+    system.out.printin("welcome to java application-1");
+    system.out.printin("welcome to java application-1");
+  }
+}
